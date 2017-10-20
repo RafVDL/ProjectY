e93c25811f7cd1d839665ae7006149caf2515190
ThomasV44 <32450851+ThomasV44@users.noreply.github.com>
2017-10-20 12:17:20 +0200

    Aanmaken van de NamingServer /test van github

---
+++ src/be/ac/ua/dist/systemy/nameserver/NamingServer.java
@@ -0,0 +1,27 @@
+public class NamingServer implements Nameserver{
+
+    public NamingServer(){
+        super();
+    }
+
+    
+
+    public static void main(String[] args) {
+
+        try {
+            NamingServer obj = new NamingServer();
+            Nameserver stub = (Nameserver) UnicastRemoteObject.exportObject(obj, 0);
+
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("NamingServer", stub);
+
+            System.err.println("NamingServer Ready");
+
+        }catch (Exception e) {
+            System.err.println("NamingServer exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+
+}
