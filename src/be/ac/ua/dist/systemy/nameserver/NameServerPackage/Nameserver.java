4aa7a6edc429a191a552be06017cd75dc9ffa547
ThomasV44 <32450851+ThomasV44@users.noreply.github.com>
2017-10-25 12:03:54 +0200

    Fouten eruit gehaald

--- src/be/ac/ua/dist/systemy/nameserver/NameServerPackage/Nameserver.java
+++ src/be/ac/ua/dist/systemy/nameserver/NameServerPackage/Nameserver.java
@@ -1,4 +1,4 @@
-package be.ac.ua.dist.systemy.nameserver;
+package be.ac.ua.dist.systemy.nameserver.NameServerPackage;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -7,7 +7,7 @@ import java.rmi.RemoteException;
 
 public interface Nameserver extends Remote {
 
-    InetAddress getOwner(String fileName) throws RemoteException;
+    InetAddress getOwner(String fileName) throws UnknownHostException, RemoteException;
     void addMeToNetwork(String computerName, InetAddress IP) throws RemoteException;
     void removeMeFromNetwork(String computerName) throws RemoteException;
 
