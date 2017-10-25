dcb881ca8d8c5aef59d333c6ba600647f041a20b
Nxt-1 <aerts.arne@gmail.com>
2017-10-25 11:59:11 +0200

    Added tcp-client in Node

--- src/be/ac/ua/dist/systemy/node/Node.java
+++ src/be/ac/ua/dist/systemy/node/Node.java
@@ -1,10 +1,17 @@
 package be.ac.ua.dist.systemy.node;
 
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public class Node implements NodeInterface {
+    public static final int PORT = 6969;
+
     private String nodeName;
     private InetAddress address;
     private int addressHash;
@@ -15,7 +22,7 @@ public class Node implements NodeInterface {
     public Node(String nodeName, InetAddress address) {
         this.nodeName = nodeName;
         this.address = address;
-        this.addressHash=Math.abs(nodeName.hashCode() % 32768);
+        this.addressHash = Math.abs(nodeName.hashCode() % 32768);
     }
 
     @Override
@@ -31,5 +38,47 @@ public class Node implements NodeInterface {
     @Override
     public List<String> getDownloadedFileList() throws RemoteException {
         return downloadedFiles;
+    }
+
+    public void downloadFile(String fileName) {
+        InetAddress remoteAddress;
+        Socket clientSocket;
+
+        try {
+            //get ip via rmi
+            remoteAddress = InetAddress.getByName("10.10.10.10");
+
+            //Open tcp socket to server @remoteAddress:port
+            clientSocket = new Socket(remoteAddress, PORT);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+            FileOutputStream fos = new FileOutputStream(fileName);
+
+            //Request file at the server
+            out.println("REQUESTFILE");
+            out.println(fileName);
+            //Initialize buffers
+            int fileSize = in.readInt();
+            byte[] buffer = new byte[1024 * 10]; // 10 kB
+            int read;
+            int remaining = fileSize;
+            //Receive the file
+            while ((read = in.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+                remaining -= read;
+                fos.write(buffer, 0, read);
+            }
+
+            //Close everything
+            fos.close();
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        //Register at server
     }
 }
