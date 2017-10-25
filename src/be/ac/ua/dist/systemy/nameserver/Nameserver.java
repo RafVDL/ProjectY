c2f3fa3574d0adaa85b12a1da86631b64637d9a1
ThomasV44 <32450851+ThomasV44@users.noreply.github.com>
2017-10-25 11:50:55 +0200

    Debugging

--- src/be/ac/ua/dist/systemy/nameserver/Nameserver.java
+++ src/be/ac/ua/dist/systemy/nameserver/Nameserver.java
@@ -1,11 +1,14 @@
 package be.ac.ua.dist.systemy.nameserver;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Nameserver extends Remote {
 
     InetAddress getOwner(String fileName) throws RemoteException;
+    void addMeToNetwork(String computerName, InetAddress IP) throws RemoteException;
+    void removeMeFromNetwork(String computerName) throws RemoteException;
 
 }
