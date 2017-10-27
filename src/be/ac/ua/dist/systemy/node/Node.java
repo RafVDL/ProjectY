70d72ecf25e70274aca2f10f695cb66c37d91d78
Nxt-1 <aerts.arne@gmail.com>
2017-10-27 11:47:17 +0200

    added discovery in Node

--- src/be/ac/ua/dist/systemy/node/Node.java
+++ src/be/ac/ua/dist/systemy/node/Node.java
@@ -19,17 +19,23 @@ import java.util.List;
 public class Node implements NodeInterface {
     public static final int PORT = 6969;
 
-    private String nodeName;
-    private InetAddress address;
-    private int addressHash;
+    private String currentName;
+    private InetAddress currentAddress;
+    private int currentHash;
     private List<String> localFiles;
     private List<String> replicatedFiles;
     private List<String> downloadedFiles;
+    private InetAddress prevAddress;
+    private InetAddress nextAddress;
+    private String prevName;
+    private String nextName;
+    private int prevHash;
+    private int nextHash;
 
     public Node(String nodeName, InetAddress address) {
-        this.nodeName = nodeName;
-        this.address = address;
-        this.addressHash = Math.abs(nodeName.hashCode() % 32768);
+        this.currentName = nodeName;
+        this.currentAddress = address;
+        this.currentHash = calculateHash(nodeName);
     }
 
     @Override
@@ -52,7 +58,7 @@ public class Node implements NodeInterface {
         Socket clientSocket;
 
         try {
-            //get ip via rmi
+            //TODO get ip via rmi
             remoteAddress = InetAddress.getByName("10.10.10.10");
 
             //Open tcp socket to server @remoteAddress:port
@@ -83,6 +89,46 @@ public class Node implements NodeInterface {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void updateNext(int newHash, InetAddress newAddress, String newName){
+        nextHash = newHash;
+        nextAddress = newAddress;
+        nextName = newName;
+    }
+
+    private void updatePrev(int newHash, InetAddress newAddress, String newName){
+        prevHash = newHash;
+        prevAddress = newAddress;
+        prevName = newName;
+    }
+
+    public void updateNeighbours(InetAddress newAddress, String newName){
+        int newHash = calculateHash(newName);
+        nextHash = calculateHash(nextName);
+        prevHash = calculateHash(prevName);
+
+        if(newHash > currentHash && newHash<nextHash){
+            // New node sits between this node en next node.
+
+            //TODO Send self and nextNode to newNode.
+            updateNext(newHash, newAddress, newName);
+        }
+        else if (newHash<currentHash&&newHash>prevHash){
+            // New node sits between this node and the previous node.
+
+            //TODO Send previous node and self to newNode.
+            updatePrev(newHash, newAddress, newName);
+        }
+        else {
+            // New node does not become a neighbour of this node.
+
+            // Do nothing
+        }
+    }
+
+    private int calculateHash(String name){
+        return Math.abs(name.hashCode() % 32768);
     }
 
     public static void main(String[] args) throws RemoteException, NotBoundException, UnknownHostException, ServerNotActiveException {
