bec205d47a886dd4f487b541693f7e2c07c3c39d
WesleyVanW <32450877+WesleyVanW@users.noreply.github.com>
2017-12-04 11:26:37 +0100

    added link to example

--- src/be/ac/ua/dist/systemy/node/FileAgent.java
+++ src/be/ac/ua/dist/systemy/node/FileAgent.java
@@ -1,6 +1,8 @@
 package be.ac.ua.dist.systemy.node;
 import java.io.Serializable;
 
+/*https://stackoverflow.com/questions/3429921/what-does-serializable-mean*/
+
 public class FileAgent implements Runnable, Serializable{
     // to run: (new Thread(new FileAgent())).start();
     public void run(){
