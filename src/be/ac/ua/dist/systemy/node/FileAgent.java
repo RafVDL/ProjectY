e30a5feed5fa08194f96a8e3b46e6cc3afd2e113
ThomasV44 <32450851+ThomasV44@users.noreply.github.com>
2017-12-04 11:24:31 +0100

    Aanmaak FileAgent

---
+++ src/be/ac/ua/dist/systemy/node/FileAgent.java
@@ -0,0 +1,12 @@
+package be.ac.ua.dist.systemy.node;
+import java.io.Serializable;
+
+public class FileAgent implements Runnable, Serializable{
+    // to run: (new Thread(new FileAgent())).start();
+    public void run(){
+
+    }
+    
+
+
+}
